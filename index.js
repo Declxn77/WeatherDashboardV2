78858de3c68f6c3ef30f3caa726909ec4f74e6a3
Declan Schembri <declanjschembri@outlook.com>
2023-04-27 19:22:23 +1000

    Added API and Setup Landing Page

---
+++ index.js
@@ -0,0 +1,18 @@
+var weatherAPIKey = "b1d9351ce87cca6dba69a03ba316e30b";
+
+//Displays List
+var geoList = $('#currentGeoList');
+
+var geoLat;
+var geoLong;
+var currentWeather;
+//Submits
+var buttonSubmit = document.getElementById('searchButton');
+
+//Displays Current Weather and Forecasts the following 5 Days
+var currentWeather = document.getElementById('currentWeather');
+var fiveDayWeather = docuement.getElementById('fiveDayWeather');
+
+
+
+
